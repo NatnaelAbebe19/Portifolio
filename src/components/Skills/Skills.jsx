98f487c90a024002991269521b383695365fb780
jud <joejud@gmail.com>
2024-05-16 23:05:38 +0300

    ADD LAMPS COMPONENT

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { LampContainer } from "./Lamp";
 
-function Skills() {
+export default function LampDemo() {
   return (
-    <div className="h-screen w-[80%] mx-auto">
-      <p className="text-white text-center">
-        Hello Those are the skills I have for the time being
-      </p>
-      <div className="text-5xl text-[#c9fd74]">Hellfdo</div>
-    </div>
+    <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        Build lamps <br /> the right way
+      </motion.h1>
+    </LampContainer>
   );
 }
-
-export default Skills;
